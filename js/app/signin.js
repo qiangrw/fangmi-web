ad218b6a75d76d1414f24869db779edead743c31
Runwei Qiang <qiangrw@gmail.com>
2015-04-19 23:55:25 +0800

    fix signin, ready for other interfaces

--- js/app/signin.js
+++ js/app/signin.js
@@ -6,8 +6,23 @@ $('#signin-page').on('pageinit', function() {
             url: config.api_url + "oauth/token",
             data: $("#signin-form").serialize(),
             success: function(data) {
-                alert(data);
-            }});
+                if (data.access_token) {
+                    $("#error").hide();
+                    config.access_token = data.access_token;
+                    localStorage.setItem('config', JSON.stringify(config));
+
+                    // TODO get user information
+                    $( ":mobile-pagecontainer" ).pagecontainer( "change", "index.html", { role: "page" } );
+                } else {
+                    $("#error").html("* 用户名密码错误");
+                    $("#error").show();
+                }
+            },
+            error: function(data) {
+                    $("#error").html("* 用户名密码错误");
+                    $("#error").show();
+                   }
+        });
     });
 });
 
