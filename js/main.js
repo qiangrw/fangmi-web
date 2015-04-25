5e57d36a2777cf6b5c7a5007ae294fa676ced75c
Runwei Qiang <qiangrw@gmail.com>
2015-04-25 23:34:26 +0800

    ready for change password

--- js/main.js
+++ js/main.js
@@ -150,6 +150,29 @@ $('#forget-password-page').on('pageinit', function() {
     });    
 });
 
+// change_password.html
+$('#change-password-page').on('pageinit', function() {
+    $("#submit-change-password").click(function(){
+        $.ajax({
+            type: 'POST',
+            url: config.api_url + "api/account/password/change",
+            beforeSend: function (request) {
+                request.setRequestHeader("Authorization", "Bearer " + user.access_token);
+            },
+            data: $("#change-password-form").serialize(),
+            success: function(data) {
+                if (data.message == 'OK') {
+                    show_common_error("修改成功，您可以用新密码登录了.");
+                } else {
+                    show_common_error(data.message);
+                }
+            },
+            error: function(data) { show_common_error('服务器错误'); }
+        });   
+
+    });    
+});
+
 // signup.html
 $('#signup-page').on('pageinit', function() {
     set_captcha_elements();
