dca9a1a60b68ca4753dca62f0fa7e44447a08377
Runwei Qiang <qiangrw@gmail.com>
2015-05-06 10:46:30 +0800

    fix message

--- js/app/message.js
+++ js/app/message.js
@@ -9,12 +9,42 @@ $("#conversation-page").on('pagebeforeshow', function() {
 });
 
  
-// message_detail.html
-$('#message-detail-page').on('pagebeforeshow', function() {
+// message.html
+$('#message-page').on('pagebeforeshow', function() {
     var from_username = getParameter("from_username");
     if (from_username == null) return;
     $("#to_username").val(from_username);
     $("#header-title").val(from_username);
+
+ 
+    var post_binding_func = function() {
+        $("#submit-post-message").unbind().click(function(){
+            console.log('clicked');
+            $.ajax({
+                type: 'POST',
+                beforeSend: function (request) {
+                    request.setRequestHeader("Authorization", "Bearer " + user.access_token);
+                },
+                url: config.api_url + "api/message",
+                data: $("#post-message-form").serialize(),
+                success: function(data) {
+                    if (data.message == 'OK') {
+                        var message =  { mine: true, content: $("#message-content-input").val() };
+                        $("#messagedetail-list").append(
+                            '<div class="message-div">' + 
+                            '<div class="message-img-div"><img src="images/user/user_profile.png" /></div>'+
+                            '<div class="message-text-div"><p>' + message.content + '</p></div>'+
+                            '</div>'
+                            );
+                        $("#message-content-input").val('');
+                    } else  {
+                        alert_message("发送失败");
+                    }
+                }, 
+                error: function(data) { alert_message("网络错误，请稍后再试"); }
+            });                 
+        });
+    };      
 
     var element = "messagedetail-list";
     $("#" + element).hide();
@@ -25,34 +55,9 @@ $('#message-detail-page').on('pagebeforeshow', function() {
             for (i = 0; i < messages.length; ++i) {
                 messages[i].mine = messages[i].from_username != from_username;
             }
-            tempo_show(element, messages);
+            tempo_show(element, messages, post_binding_func);
         }
     });
 
-    $("#submit-post-message").click(function(){
-        $.ajax({
-            type: 'POST',
-            beforeSend: function (request) {
-                request.setRequestHeader("Authorization", "Bearer " + user.access_token);
-            },
-            url: config.api_url + "api/message",
-            data: $("#post-message-form").serialize(),
-            success: function(data) {
-                if (data.message == 'OK') {
-                    var message =  { mine: true, content: $("#message-content-input").val() };
-                    $("#messagedetail-list").append(
-                        '<div class="message-div">' + 
-                        '<div class="message-img-div"><img src="images/user/user_profile.png" /></div>'+
-                        '<div class="message-text-div"><p>' + message.content + '</p></div>'+
-                        '</div>'
-                        );
-                    $("#message-content-input").val('');
-                } else  {
-                    alert_message("发送失败");
-                }
-            }, 
-            error: function(data) { alert_message("网络错误，请稍后再试"); }
-        });                 
-    });
 });  
 
